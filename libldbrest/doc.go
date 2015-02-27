83cf3edce70a2e7ab22ac48947cb794748cad37b
Travis J Parker <travis.parker@gmail.com>
2015-02-27 13:40:35 -0800

    have godoc make that a link

--- libldbrest/doc.go
+++ libldbrest/doc.go
@@ -1,5 +1,5 @@
 /*
 libldbrest contains all the logic implementing the REST server
-described in godoc.org/github.com/teepark/ldbrest.
+described in http://godoc.org/github.com/teepark/ldbrest.
 */
 package libldbrest
